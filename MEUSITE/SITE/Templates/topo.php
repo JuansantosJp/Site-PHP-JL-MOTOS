89384d444db71bbff18c1afab49f61d304e60422
gilvann0 <gilvannetom@gmail.com>
2024-11-14 20:51:06 -0300

    Update topo.php
    Tentando adicionar a imagem

--- MEUSITE/SITE/Templates/topo.php
+++ MEUSITE/SITE/Templates/topo.php
@@ -1,3 +1,5 @@
+<img src="https://www.google.com/imgres?q=mecanico%20moto&imgurl=https%3A%2F%2Fescotec.com.br%2Fwp-content%2Fuploads%2F2023%2F01%2FCurso-Mecanica-e-Eletricidade-de-Motos-2.0.jpg&imgrefurl=https%3A%2F%2Fescotec.com.br%2Fproduto%2Fmecanica-e-eletrica-de-motos%2F&docid=ngmoQjKu75jqfM&tbnid=yubSBkcWDPZSAM&vet=12ahUKEwiVp-iegN2JAxVkrZUCHQ0gMXMQM3oECGYQAA..i&w=2560&h=1706&hcb=2&ved=2ahUKEwiVp-iegN2JAxVkrZUCHQ0gMXMQM3oECGYQAA" alt="Uma imagem de exemplo">
 <?php
 
 echo"<h1>⚒JL MOTOS</h1>";
+?>
