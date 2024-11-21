29146d7ff1db466ac220dbcba1f31adbc431272c
Vinicius60 <lucasviniciusdasilvafigueiredo@gmail.com>
2024-11-21 01:38:06 -0300

    Create form_cliente.php
    Criação de cadastro de clientes

---
+++ MEUSITE/admin/form_cliente.php
@@ -0,0 +1,9 @@
+<h3> Novo Cliente </h3>
+<form action="?pg=cadastra_cliente" method="post">
+    Nome: <input type="text" name="nome"><br>
+    E-mail: <input type="text" name="email"><br> 
+    Assunto: <input type="text" name="assunto"><br> 
+    Mensagem: <input type="text" name="Mensagem"><br>
+
+    <input type="submit" value="CADASTRAR">
+</form>
